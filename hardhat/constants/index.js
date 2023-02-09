5f93cfec18d4f6d6b716a6c21e94ff2cef2e36f6
christianaojo <christianaojoofficial.com>
2023-02-09 12:41:34 +0100

    first commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xFB6779ea4c7548550c4eEc39c0D7A63b583a81e1";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
